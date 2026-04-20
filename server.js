3e50716e6e84a77bcb5d26c54429f602f74c57bc
Sukrutaraj <deepak_sukraj@hotmail.com>
2026-04-20 14:18:11 +0200

    server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import WebSocket from "ws";
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+const server = app.listen(PORT, () => {
+  console.log("Server running");
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (client) => {
+
+  const deepgram = new WebSocket(
+    "wss://api.deepgram.com/v1/listen?punctuate=false&language=sv",
+    {
+      headers: {
+        Authorization: `Token ${process.env.DEEPGRAM_API_KEY}`
+      }
+    }
+  );
+
+  deepgram.on("message", (msg) => {
+    try {
+      const data = JSON.parse(msg.toString());
+      const transcript = data.channel?.alternatives?.[0]?.transcript;
+
+      if (transcript) {
+        client.send(transcript);
+      }
+    } catch {}
+  });
+
+  client.on("message", (audio) => {
+    if (deepgram.readyState === WebSocket.OPEN) {
+      deepgram.send(audio);
+    }
+  });
+
+  client.on("close", () => {
+    deepgram.close();
+  });
+});
