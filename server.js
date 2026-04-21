82a2e92507f72ed29c0617ad87f283225562a850
Sukrutaraj <deepak_sukraj@hotmail.com>
2026-04-21 11:46:43 +0200

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const WebSocket = require("ws");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+app.use(cors());
 app.use(express.json());
 
 // 🧠 NAMN LAGRING (i minne just nu)
