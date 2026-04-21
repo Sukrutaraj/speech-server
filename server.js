fe5457cea3764eed006d6f9dc40b78ee012f50f8
Sukrutaraj <deepak_sukraj@hotmail.com>
2026-04-21 15:49:35 +0200

    server.js

--- server.js
+++ server.js
@@ -51,47 +51,6 @@ app.post("/names", (req, res) => {
   res.send({ success: true });
 });
 
-// 🟢 TEST
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
-
-const server = app.listen(PORT, () => {
-  console.log("Server running");
-});
-
-// 🔌 WEBSOCKET (din speech del)
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", (client) => {
-
-  const deepgram = new WebSocket(
-    "wss://api.deepgram.com/v1/listen?punctuate=false&language=sv",
-    {
-      headers: {
-        Authorization: `Token ${process.env.DEEPGRAM_API_KEY}`
-      }
-    }
-  );
-
-  deepgram.on("message", (msg) => {
-    try {
-      const data = JSON.parse(msg.toString());
-      const transcript = data.channel?.alternatives?.[0]?.transcript;
-
-      if (transcript) {
-        client.send(transcript);
-      }
-    } catch {}
-  });
-
-  client.on("message", (audio) => {
-    if (deepgram.readyState === WebSocket.OPEN) {
-      deepgram.send(audio);
-    }
-  });
-
-  client.on("close", () => {
-    deepgram.close();
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
